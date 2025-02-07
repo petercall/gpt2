1c54054329b0d5b4c1a27fd4956e52f8b9e69890
petercall <peter.t.call@gmail.com>
2025-02-07 16:36:29 -0600

    Finished bigram model.

--- tester.py
+++ tester.py
@@ -1,9 +1,6 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from matplotlib import pyplot as plt
+import numpy as np
 
-tens1 = torch.tensor([[1,2], [3,4]])
-tens2 = torch.tensor([[5,6], [7,8]])
-tens3 = torch.tensor([[9,10], [11,12]])
-
-new_tens = torch.stack([tens1, tens2, tens3], dim = 2)
-print(new_tens.shape)
-print(new_tens)
