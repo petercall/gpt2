8c2e1a05b68bb8ea6bb99f2d016b68872481fbdf
petercall <peter.t.call@gmail.com>
2025-03-05 12:04:38 -0600

    Worked on `generate_data.py`

--- generate_data.py
+++ generate_data.py
@@ -4,25 +4,73 @@ import numpy as np
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import pipeline
 from tqdm import tqdm
+import torch
+import numpy as np
+
+#Hyperparameters---------------------------------------------------------------------------------------------------------------------------------------------------------------
+data_location = "data/subjects/train.csv"
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model_name = "microsoft/Phi-3-mini-4k-instruct"    # model_name = "mistralai/Mistral-7B-Instruct-v0.3"     
+batch_size = 2
+generation_args = {
+    "max_new_tokens" : 300,
+    "do_sample" : True,
+}
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-device = "cuda" # the device to load the model onto
 
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
 
-messages = [
-    {"role": "user", "content": "What is your favourite condiment?"},
-    {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
-    {"role": "user", "content": "Do you have mayonnaise recipes?"}
+#Load in the data
+data = pd.read_csv(data_location)
+# print(data.head())
+
+#Load in the model and the tokenizer and put the model onto the gpu
+model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side = "left")
+tokenizer.padding_side = "left"
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.bos_token
+
+#Set the request you want the model to do and the chat-message format
+# request = "Write me a paragraph that might be found in a textbook before the following text"
+role = "You are an AI writer"
+message = [
+    {"role": "user", "content": role},
+    {"role": "user", "content": ""}
 ]
 
-encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
+#Set the number of loops you need to perform
+# n = data.shape[0]
+n = 2
+iters = n // batch_size
+extra = n - (iters * batch_size)
+iters = iters + 1 if extra != 0 else iters
 
-model_inputs = encodeds.to(device)
-model.to(device)
+for i in range(iters):
+    messages = []
+    if extra != 0 and i == iters - 1:
+        internal_iters = extra
+    else:
+        internal_iters = batch_size
+    print()
 
-generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
-decoded = tokenizer.batch_decode(generated_ids)
-print(decoded[0])    
-    
+    import copy
+    for j in range(internal_iters):
+        current_message = copy.deepcopy(message)
+        current_message[1]["content"] = data.at[(i*batch_size) + j, "generation"]
+        messages.append(current_message)
+
+    my_dict = tokenizer(tokenizer.apply_chat_template(messages, tokenize = False), add_special_tokens = False, return_tensors = "pt", padding = True)
+    input_ids = my_dict["input_ids"].to(device)
+    attention_mask = my_dict['attention_mask'].to(device)
+    print(input_ids)
+
+    generated_ids = model.generate(input_ids, attention_mask = attention_mask, **generation_args)
+
+    batch = tokenizer.batch_decode(generated_ids, skip_special_tokens = True)
+    print()
+    print(batch[0])
+    print()
+
+
--- tester.py
+++ tester.py
@@ -9,23 +9,7 @@ import pandas as pd
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
+device = torch.device("cuda") if torch.cuda.is_availble() else torch.device("cpu")
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
 
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3").to(device)
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3")
-
-df = pd.read_csv("data/subjects/train.csv")
-message = df.at[0, "generation"]
-print(message)
-
-messages = [
-    {"role": "user", "content": message}
-]
-
-model_inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to(device)
-
-generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
-print(generated_ids)
-# decoded = tokenizer.batch_decode(generated_ids)
-# print(decoded[0])
